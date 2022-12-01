05d8f5450fecd6a4905831b384fb2a4d5a7fe16c
Adem Erbas <ae@netsense.no>
2022-12-01 01:55:00 +0100

    php-login-form

--- php-login-form/loginVersion3/home.php
+++ php-login-form/loginVersion3/home.php
@@ -1,5 +1,12 @@
 <!-- Restricting Access to Pages
-Most websites where users are asked to register have some other pages where users access and store private data. You can use session variables to protect these pages. If the session variable isn't set, simply redirect the users to the login page. Otherwise, show them the contents of the page. -->
+Most websites where users are asked to register have some other pages where users access and store private data. You can use session variables to protect these pages. If the session variable isn't set, simply redirect the users to the login page. Otherwise, show them the contents of the page.
+
+KULLANICI ADRESTEN DIREK DUSMEYE CALISIRSA BU SAYFAYA ONU HEMEN SESSION DAN KONTROL EDIP HOOOP DIYECEGIZ YOKSA...DIREK BIZIM COK KRITIK DATALARIMIZA ERSMIS OLACAKK.....
+
+
+-->
+
+
 
 
 <?php
@@ -9,6 +16,20 @@ Most websites where users are asked to register have some other pages where user
         exit;
     } else {
         // Show users the page
-        echo "WELCOME TO THE HOMEPAGE";
+        echo "<h2>WELCOME TO THE HOMEPAGE ".$_SESSION["name"]. "</h2>";
+        
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="logout.php">LOGOUT</a>
+</body>
+</html>
